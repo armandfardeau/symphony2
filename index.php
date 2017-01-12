3126160c77d4168eacfb0765e231d6051429fd46
Armand Fardeau <fardeauarmand@gmail.com>
2017-01-12 15:58:20 +0100

    correct namespace

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once "vendor/autoload.php";
+
+$toto = new Acme\Toto();
+
---
+++ src/acme/toto.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: armandfardeau
+ * Date: 12/01/2017
+ * Time: 14:45
+ */
+
+namespace acme;
+
+
+class toto
+{
+public function __construct()
+{
+    echo "Singleton";
+}
+}
